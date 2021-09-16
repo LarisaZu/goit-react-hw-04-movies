672465343e05e433808d65cd5aab51031c81d789
LarisaZu <zueval@icloud.com>
2021-09-16 10:46:00 +0300

    fix

--- src/views/ReviewsView.js
+++ src/views/ReviewsView.js
@@ -4,7 +4,7 @@ import { fetchMovieReviews } from '../services/movies-api';
 
 const ReviewsView = () => {
     const { movieId } = useParams();
-    const [reviews, setReviews] = useState();
+    const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
         fetchMovieReviews(movieId).then(response =>
@@ -12,10 +12,13 @@ const ReviewsView = () => {
             setReviews(response.results),
         );
     }, [movieId]);
-
+    console.log('movieId', movieId);
+    console.log('reviews', reviews);
     return (
         <>
-            {reviews && reviews.length > 0 ? (
+            {reviews.length === 0 ? (
+                <p>We don't have any reviews for this movie</p>
+            ) : (
                 <ul>
                     {reviews.map(review => (
                         <li key={review.id}>
@@ -24,8 +27,6 @@ const ReviewsView = () => {
                         </li>
                     ))}
                 </ul>
-            ) : (
-                "We don't have any reviews for this movie"
             )}
         </>
     );
